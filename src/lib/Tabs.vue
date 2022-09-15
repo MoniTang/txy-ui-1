02328c56fff86a87ec17255d9dd13d4276c34382
tangxy <istangxy@163.com>
2022-09-15 20:50:44 +0800

    优化

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -2,7 +2,7 @@
     <div class="txy-tabs">
         <div class="txy-tabs-nav" ref="container">
             <div class="txy-tabs-nav-item" v-for="(t,index) in titles"
-            :ref="el=>{if(el) navItems[index]=el}" 
+            :ref="el=>{if(t===selected) selectItem=el}" 
              :class="{selected: t===selected}" @click="select(t)"
             :key="index">{{t}}</div>
             <div class="txy-tabs-nav-indicator" ref="indicator"></div>
@@ -16,40 +16,38 @@
 </template>
 
 <script lang="ts">
-import { onMounted, onUpdated, ref } from 'vue'
+import { onMounted, ref, watchEffect } from 'vue'
 import Tab from './Tab.vue'
     export default {
      props: {
     selected: {type: String}
         },
     setup(props,context){
-        const navItems=ref<HTMLDivElement[]>([])
+        const selectItem=ref<HTMLDListElement>(null)
         const indicator=ref<HTMLDivElement>(null)
         const container=ref<HTMLDivElement>(null)
-        const x=(()=>{
-            const divs=navItems.value
-            const result=divs.filter(
-                div=>div.classList.contains('selected'))[0]            
-                const {width}=result.getBoundingClientRect()
-                indicator.value.style.width=width+'px'
-                const {left:left1}=container.value.getBoundingClientRect()
-                const {left:left2}=result.getBoundingClientRect()
-                const left=left2-left1
-                indicator.value.style.left=left+'px'
-            })
-        onMounted(x)
-        onUpdated(x)
-            const defaults=context.slots.default()
-            defaults.forEach((tag)=>{
-                if(tag.type!==Tab){throw new Error('Tabs子标签必须是Tab')}
-            })
-            const titles = defaults.map( (tag)=>{
-                    return tag.props.title
+       onMounted(()=>{
+        watchEffect(()=>{
+            const {width}=selectItem.value.getBoundingClientRect()
+            indicator.value.style.width=width+'px'
+            const {left:left1}=container.value.getBoundingClientRect()
+            const {left:left2}=selectItem.value.getBoundingClientRect()
+            const left=left2-left1
+            indicator.value.style.left=left+'px'
+        })
+       })
+        const defaults=context.slots.default()
+            defaults.forEach((tag) => {
+                if (tag.type !== Tab) {
+                    throw new Error('Tabs 子标签必须是 Tab')}
                 })
-            const select = (title: string) => {
-                context.emit('update:selected', title)
-                }
-            return {indicator,defaults,titles,select,navItems,container}
+        const titles = defaults.map( (tag)=>{
+            return tag.props.title
+            })
+        const select = (title: string) => {
+            context.emit('update:selected', title)
+             }
+            return {indicator,defaults,titles,select,selectItem,container}
         }    
     }
 </script>
