8145e9af6e4ade61ed1229eb340de8a42492b879
tangxy <istangxy@163.com>
2022-09-15 16:20:06 +0800

    渲染Tab

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -2,12 +2,18 @@
     <div>
 tabsDemo
     </div>
+<Tabs>
+    <Tab title="导航1">1</Tab>
+    <Tab title="导航2">2</Tab>
+</Tabs>
 </template>
 
-<script>
+<script lang="ts">
+import Tab from '../lib/Tab.vue';
+import Tabs from '../lib/Tabs.vue';
     export default {
-        
-    }
+    components: { Tab, Tabs }
+}
 </script>
 
 <style lang="scss" scoped>
