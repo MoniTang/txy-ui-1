f70c321f6979ab6f7ddac724a2e790dec05343ca
tangxy <istangxy@163.com>
2022-09-18 00:30:08 +0800

    优化了markdown

--- src/router.ts
+++ src/router.ts
@@ -1,18 +1,16 @@
 import { createWebHashHistory, createRouter } from "vue-router"
 import Home from './view/Home.vue'
 import Doc from './view/Doc.vue'
-import Intro from './view/Intro.vue'
-import Install from './view/Install.vue'
-import GetStarted from './view/GetStarted.vue'
 
 import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import DocDemo from './components/DocDemo.vue'
+import { h } from 'vue';
+import Markdown from './components/Markdown.vue';
 
-
-
+const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
 const history = createWebHashHistory()
 export const router = createRouter({
     history: history,
@@ -21,9 +19,9 @@ export const router = createRouter({
         { path: '/doc', component: Doc ,
         children:[
             {path:'/',component:DocDemo},
-            {path:'intro',component:Intro},
-            {path:'install',component:Install},
-            {path:'get-start',component:GetStarted},
+            { path: "intro", component: md('intro') },
+            { path: "get-start", component: md('get-start') },
+            { path: "install", component: md('install') },
             {path:'switch',component:SwitchDemo},
             {path:'button',component:ButtonDemo},
             {path:'dialog',component:DialogDemo},
