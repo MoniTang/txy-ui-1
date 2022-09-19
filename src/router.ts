0d08ae7661795a3c8f74bf4cc16aea659d3f7765
tangxy <istangxy@163.com>
2022-09-19 17:43:00 +0800

    修改Markdown

--- src/router.ts
+++ src/router.ts
@@ -6,10 +6,14 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
+
 import { h } from 'vue';
 import Markdown from './components/Markdown.vue';
+import intro from './markdown/intro.md'
+import getStarted from './markdown/get-start.md'
+import install from './markdown/install.md'
 
-const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
+const md = string => h(Markdown, { content: string, key: string })
 const history = createWebHashHistory()
 export const router = createRouter({
     history: history,
@@ -18,9 +22,9 @@ export const router = createRouter({
         { path: '/doc', component: Doc ,
         children:[
             {path:'',redirect:'/doc/intro'},
-            { path: "intro", component: md('intro') },
-            { path: "get-start", component: md('get-start') },
-            { path: "install", component: md('install') },
+            { path: "intro", component: md(intro) },
+            { path: "get-start", component: md(getStarted) },
+            { path: "install", component: md(install) },
             {path:'switch',component:SwitchDemo},
             {path:'button',component:ButtonDemo},
             {path:'dialog',component:DialogDemo},
